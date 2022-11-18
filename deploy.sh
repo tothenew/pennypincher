9cd56e4dd0ffb10f333a93a4cce9e95bdf490cbc
Yash Khandelwal <yash.khandelwal.inbox@gmail.com>
2022-11-18 15:19:31 +0530

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,3 +0,0 @@
-docker build -t pennypincher .
-docker tag pennypincher:latest 743930152640.dkr.ecr.us-east-1.amazonaws.com/pennypincher:demo$1
-docker push 743930152640.dkr.ecr.us-east-1.amazonaws.com/pennypincher:demo$1
