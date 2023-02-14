e4b4fa7c629db9abff47afc7345beb2f93184ee9
HAJAREPRIYANKA <priyanka.hajare2700@gmail.com>
2023-02-14 16:35:02 +0530

    html templated editing

--- main.py
+++ main.py
@@ -80,7 +80,8 @@ def lambda_handler(event=None, context=None):
         os.makedirs(dir_path,exist_ok=True)
         html_path = dir_path+ '/pennypincher_findings.html'
         header = '<h3><b>Cost Optimization Report |  ' + account_name + ' | Total Savings: $'+ str(round(total_savings, 2)) + '</h3></b>'
-        html = header + html
+        logo = '<img src="pennypincher-logo.png" height="200" width="200" >'
+        html = logo + header + html
         with FileManager(html_path, 'w') as f:
             f.write(html)
         date_obj = date.today()
